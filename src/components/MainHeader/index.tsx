7765b68ea580bf9766c5dbb0ceee4cf4120c76a3
Ezequiel Lorenzatti <ezelorenzatti@gmail.com>
2023-04-09 19:49:23 -0300

    Estrututa base do projeto Minha Carteria

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const MainHeader: React.FC = () => {
+  return <Container>
+        <div >MainHeader</div>
+  </Container>
+  
+}
+
+export default MainHeader;

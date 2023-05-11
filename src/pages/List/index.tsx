ec7d75917abd5a32ccaf2d26513107a89e03c8b8
Ezequiel Lorenzatti <ezelorenzatti@gmail.com>
2023-05-11 19:42:49 -0300

    Filtro por tipo de despesa

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,21 +1,52 @@
 import React from "react";
-import { Container, Content } from "./syles";
+import { Container, Content, Filters } from "./syles";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () =>{
 
-    const ops = [
-        {value:'op1', label:'Op1'},
-        {value:'op2', label:'Op2'}
+    const months = [
+        {value:1, label:'Janeiro'},
+        {value:2, label:'Fevereiro'},
+        {value:3, label:'Março'},
+        {value:4, label:'Abril'},
+        {value:5, label:'Maio'},
+        {value:6, label:'Junho'},
+        {value:7, label:'Julho'},
+        {value:8, label:'Agosto'},
+        {value:9, label:'Setembro'},
+        {value:10, label:'Outubro'},
+        {value:11, label:'Novembro'},
+        {value:12, label:'Dezembro'}
+    ];
+
+    
+    const years = [
+        {value:2023, label:2023},
+        {value:2022, label:2022},    
+        {value:2021, label:2021},    
+        {value:2020, label:2020}    
     ];
 
     return (
         <Container>
-            <ContentHeader title="Saídas" lineColor="#FFF">
-                <SelectInput options={ops}/>            
+            <ContentHeader title="Saídas" lineColor="#E44C4E">
+                <SelectInput options={months}/>            
+                <SelectInput options={years}/>            
             </ContentHeader>
+
+            <Filters>
+                <button type="button"
+                    className="tag-filter tag-filter-recurrent">
+                    Recorrentes
+                </button>
+
+                <button type="button"
+                    className="tag-filter tag-filter-eventual">
+                    Eventuais
+                </button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard 
